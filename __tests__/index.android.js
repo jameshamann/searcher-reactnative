fdd356627386e04a28e0435cae6d9e9f2be38904
jameshamann <jameshamann@me.com>
2017-03-20 16:53:32 +0000

    sets up tests correctly, still working on mocking fetch api call

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -11,3 +11,16 @@ it('renders correctly', () => {
   ).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+//I don't even know why the hell I kept this in.
+
+it('displays searcher on the page', () =>{
+  const tree = renderer.create(
+    <Index />
+  ).toJSON();
+  expect(tree.children[0].children).toEqual(["Searcher"])
+});
+
+it('displays movie data after submitting a title', () => {
+
+});
