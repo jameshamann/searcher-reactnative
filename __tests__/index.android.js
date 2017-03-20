c82a8dc831682f5b8147da32a767350d723241bc
jameshamann <jameshamann@me.com>
2017-03-20 11:32:24 +0000

    sets up tests using jest

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -8,5 +8,6 @@ import renderer from 'react-test-renderer';
 it('renders correctly', () => {
   const tree = renderer.create(
     <Index />
-  );
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
 });
